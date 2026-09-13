3f3e0edf256b6ea8ca602b14a2a189e8f40978cc
Siddharth Singh <siddharthrajpoot08@gmail.com>
2026-09-13 15:59:48 +0530

    Time: 2 ms (98.62%) | Memory: 85.9 MB (25.66%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+      boolean[] seen = new boolean[nums.length];
+
+for(int i = 0; i < nums.length; i++) {
+
+    if(seen[nums[i]] == true) {
+        return nums[i];
+    }
+
+    seen[nums[i]] = true;
+}
+return -1;
+    }
+}
